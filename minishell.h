f390a19850525a1b4c4b54f833acaa205b82ffd8
Joon Yeol Lee <joon-lee@c2r3s3.42seoul.kr>
2023-04-10 12:22:00 +0900

    joon-lee

---
+++ minishell.h
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: joon-lee <joon-lee@student.42seoul.>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/10 12:06:54 by joon-lee          #+#    #+#             */
+/*   Updated: 2023/04/10 12:06:56 by joon-lee         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+#include <stdlib.h>
+#include <unistd.h>
+#include <readline/readline.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <sys/ioctl.h>
+#include <dirent.h>
+#include <termios.h>
+#include <term.h>
+
+typedef enum e_type
+{
+	CMD, REDIR, PIPE, SEP, CTR_OP
+}	t_type;
+
+typedef struct s_node
+{
+	enum e_type		type;
+	char 			*data;
+	struct s_token	*left;
+	struct s_token	*right;
+}	t_node;
+
+
+
+#endif
---
+++ tree_ops.c
@@ -0,0 +1,9 @@
+#include "minishell.h"
+
+t_node	*insert_token(t_node *root, char *data)
+{
+	if (!root)
+	{
+		root = (t_node *)malloc
+	}
+}
