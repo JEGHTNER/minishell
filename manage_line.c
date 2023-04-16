a8cd75d1ede11c541b4a9a3ba2e07ad4bd6d6ee0
joon-lee <naanthonylee@gmail.com>
2023-04-16 18:37:14 +0900

    joon-lee

--- manage_line.c
+++ manage_line.c
@@ -23,6 +23,12 @@ void	manage_pipe(t_cmd *cmd, char *line, size_t *idx, size_t *quote)
 void	manage_quotation(t_cmd *cmd, char *line, size_t *idx, size_t *pipe)
 {
 
+	//쌍따옴표 안에서 환경변수($), 한따옴표만 특수 처리, 나머지 문자열 출력
+	//한따옴표 안의 한따옴표는 따옴표 출력 안 하고 나머지는 문자열로 출력하기
+	//따옴표 갯수 쌍 맞는지 확인
 }
 
-void	manage_chunk(t_cmd *cmd, char *line, size_t *idx);
+void	manage_chunk(t_cmd *cmd, char *line, size_t *idx)
+{
+	
+}
