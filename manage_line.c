5a79c46fa1681218bbb96d859e43e6aaa8689851
Joon Yeol Lee <joon-lee@c2r3s7.42seoul.kr>
2023-04-17 19:52:45 +0900

    joon-lee

--- manage_line.c
+++ manage_line.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   manage_line.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: joon-lee <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/17 15:26:54 by joon-lee          #+#    #+#             */
+/*   Updated: 2023/04/17 15:26:55 by joon-lee         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "minishell.h"
 
 void	manage_pipe(t_cmd *cmd, char *line, size_t *idx, size_t *quote)
@@ -6,7 +18,7 @@ void	manage_pipe(t_cmd *cmd, char *line, size_t *idx, size_t *quote)
 
 
 	if (cmd->chunk_head == 0)
-		ft_exit_with_error("syntax error near unexpected token", "`|'");	
+		ft_exit_with_error("syntax error near unexpected token", "`|'");
 	else
 	{
 		if (line[*idx + 1] == '|')
@@ -20,15 +32,52 @@ void	manage_pipe(t_cmd *cmd, char *line, size_t *idx, size_t *quote)
 	}
 }
 
-void	manage_quotation(t_cmd *cmd, char *line, size_t *idx, size_t *pipe)
+static size_t	check_side_quotation(char *line, size_t start)
+{
+	size_t	end_idx;
+	char	quote;
+	int 	flag;
+
+	end_idx = ft_strlen(line) - 1;
+	quote = line[start];
+	if (line[start] == '\'')
+		flag = 0;
+	else
+		flag = 1;
+	while (start <= end_idx)
+	{
+		if (line[end_idx] == quote)
+			break ;
+		end_idx--;
+	}
+	if (start == end_idx)
+		ft_exit_with_error("syntax error: side quotation didn't match", 0);
+	return (end_idx);
+}
+
+static void		check_quotation_match(char *line, size_t start, size_t end)
 {
 
-	//쌍따옴표 안에서 환경변수($), 한따옴표만 특수 처리, 나머지 문자열 출력
-	//한따옴표 안의 한따옴표는 따옴표 출력 안 하고 나머지는 문자열로 출력하기
-	//따옴표 갯수 쌍 맞는지 확인
+}
+
+void	manage_quotation(t_cmd *cmd, char *line, size_t *idx, size_t *pipe)
+{
+	char	*data;
+	int 	flag;
+	size_t	end_idx;
+	char	quote;
+
+	end_idx = check_side_quotation(line, *idx);
+	check_quotation_match(line, *idx, end_idx);
+	quote = line[*idx];
+	while(1)
+	{
+
+	}
+
 }
 
 void	manage_chunk(t_cmd *cmd, char *line, size_t *idx)
 {
-	
+
 }
