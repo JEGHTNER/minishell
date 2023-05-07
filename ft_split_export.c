a4abc0cc34cd87a8107e6609f074db6ea27097ba
jeghtner <jeheon0717@naver.com>
2023-05-08 02:58:53 +0900

    <refactor> : split_export norm fix

--- ft_split_export.c
+++ ft_split_export.c
@@ -3,68 +3,70 @@
 /*                                                        :::      ::::::::   */
 /*   ft_split_export.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jehelee <jehelee@student.42.fr>            +#+  +:+       +#+        */
+/*   By: jehelee <jehelee@student.42.kr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/18 14:24:19 by jehelee           #+#    #+#             */
-/*   Updated: 2023/05/05 18:59:53 by jehelee          ###   ########.fr       */
+/*   Updated: 2023/05/08 02:57:47 by jehelee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-// static char	**free_all(char **words)
-// {
-// 	int	i;
-
-// 	i = 0;
-// 	while (words[i])
-// 	{
-// 		free(words[i]);
-// 		i++;
-// 	}
-// 	free(words);
-// 	return (0);
-// }
-
-static int	check_sep(char c, char sep)
+static char	**free_words(char **words)
 {
-	if (c == sep)
-		return (1);
+	int	i;
+
+	i = 0;
+	while (words[i])
+	{
+		free(words[i]);
+		i++;
+	}
+	free(words);
 	return (0);
 }
 
-static int	count_words(char const *string, char seperator)
+static	char	**case_no_equal(char const *string, int i)
 {
-	int	i;
-	int	count;
+	char	**words;
 
-	i = 0;
-	count = 0;
-	while (string[i])
+	if (ft_strlen(string) == i)
 	{
-		while (string[i] && check_sep(string[i], seperator))
-			i++;
-		if (string[i])
-			count++;
-		while (string[i] && !check_sep(string[i], seperator))
-			i++;
+		words = malloc(sizeof(char *) * 2);
+		if (!words)
+			return (0);
+		words[0] = malloc(sizeof(char) * i + 1);
+		if (!words[0])
+			return (free_words(words));
+		ft_strlcpy(words[0], string, i + 1);
+		words[1] = NULL;
+		return (words);
 	}
-	return (count);
+	return (NULL);
 }
 
-static char	*make_string(char const *string, char seperator)
+static	char	**case_eqaul(char const *string, int i)
 {
-	char	*tmp_string;
+	char	**words;
+	int		j;
 	int		len;
 
-	len = 0;
-	while (string[len] && string[len] != seperator)
-		len++;
-	tmp_string = malloc(sizeof(char) * len + 1);
-	if (!tmp_string)
-		return (0);
-	ft_strlcpy(tmp_string, string, len + 1);
-	return (tmp_string);
+	words = malloc(sizeof(char *) * 3);
+	if (!words)
+		return (NULL);
+	len = i;
+	words[0] = malloc(sizeof(char) * len + 1);
+	if (!words[0])
+		return (free_words(words));
+	ft_strlcpy(words[0], string, len + 1);
+	while (string[++i])
+		j++;
+	words[1] = malloc(sizeof(char) * j + 1);
+	if (!words[1])
+		return (free_words(words));
+	ft_strlcpy(words[1], &string[len + 1], j + 1);
+	words[2] = 0;
+	return (words);
 }
 
 char	**ft_split_export(char const *string, char seperator)
@@ -81,26 +83,8 @@ char	**ft_split_export(char const *string, char seperator)
 		if (string[i] == seperator)
 			break ;
 	}
-	if (ft_strlen(string) == i)
-	{
-		words = malloc(sizeof(char *) * 2);
-		if (!words)
-			return (0);
-		words[0] = malloc(sizeof(char) * i + 1);
-		ft_strlcpy(words[0], string, i + 1);
-		words[1] = NULL;
+	words = case_no_equal(string, i);
+	if (words != NULL)
 		return (words);
-	}
-	words = malloc(sizeof(char *) * 3);
-	if (!words)
-		return (0);
-	len = i;
-	words[0] = malloc(sizeof(char) * len + 1);
-	ft_strlcpy(words[0], string, len + 1);
-	while (string[++i])
-		j++;
-	words[1] = malloc(sizeof(char) * j + 1);
-	ft_strlcpy(words[1], &string[len + 1], j + 1);
-	words[2] = 0;
-	return (words);
+	return (case_eqaul(string, i));
 }
