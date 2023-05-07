ea062840314e2937db5febff635165d3426510fe
jeghtner <jeheon0717@naver.com>
2023-05-08 04:08:47 +0900

    <refactor> : cd function norm fix

--- built_in_func2.c
+++ built_in_func2.c
@@ -6,52 +6,67 @@
 /*   By: jehelee <jehelee@student.42.kr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/16 14:09:45 by jehelee           #+#    #+#             */
-/*   Updated: 2023/05/08 03:07:26 by jehelee          ###   ########.fr       */
+/*   Updated: 2023/05/08 04:08:21 by jehelee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	cd(t_cmd *cmd, char **argv)
+int	cd_home_check(t_cmd *cmd, char **argv)
 {
-	char		path[1024];
-	char		*new_path;
-	char		*tmp;
-	t_env_lst	*old_pwd;
-	t_env_lst	*pwd;
-
 	if (!argv[1])
 	{
 		if (chdir(find_value_with_key(cmd->env_head, "HOME")) == -1)
 		{
 			exit_status = 1;
-			printf("minishell: cd: HOME not set\n");
+			ft_putstr_fd("minishell: cd: HOME not set\n", 2);
 		}
-		exit_status = 0;
-		return ;
+		return (1);
 	}
-	if (argv[1][0] == '/')
-	{
-		if (chdir(argv[1]) == -1)
-		{
-			ft_putstr_fd("minishell: cd: ", 2);
-			perror(argv[1]);
-			exit_status = 1;
-			return ;
-		}
-		if (getcwd(path, 1024) == NULL)
-			perror("getcwd error\n");
-		pwd = find_env(cmd, "PWD");
-		old_pwd = find_env(cmd, "OLDPWD");
-		if (old_pwd == NULL)
-			add_env_list(cmd, pwd->key, pwd->value);
-		free(old_pwd->value);
-		old_pwd->value = ft_strdup(pwd->value);
-		free(pwd->value);
-		pwd->value = ft_strdup(path);
-		exit_status = 0;
-		return ;
-	}
+	return (0);
+}
+
+void	refresh_path(char *path, t_cmd *cmd, char *new_path)
+{
+	t_env_lst	*old_pwd;
+	t_env_lst	*pwd;
+
+	if (getcwd(path, 1024) == NULL)
+		perror("getcwd error\n");
+	pwd = find_env(cmd, "PWD");
+	old_pwd = find_env(cmd, "OLDPWD");
+	free(pwd->value);
+	pwd->value = ft_strdup(path);
+	chdir(new_path);
+	free(new_path);
+	exit_status = 0;
+}
+
+void	check_a_path(char *path, t_cmd *cmd)
+{
+	t_env_lst	*old_pwd;
+	t_env_lst	*pwd;
+
+	if (getcwd(path, 1024) == NULL)
+		perror("getcwd error\n");
+	pwd = find_env(cmd, "PWD");
+	old_pwd = find_env(cmd, "OLDPWD");
+	if (old_pwd == NULL)
+		add_env_list(cmd, pwd->key, pwd->value);
+	free(old_pwd->value);
+	old_pwd->value = ft_strdup(pwd->value);
+	free(pwd->value);
+	pwd->value = ft_strdup(path);
+	exit_status = 0;
+	return ;
+}
+
+int	check_r_path(char *path, t_cmd *cmd, char **argv, char *new_path)
+{
+	char		*tmp;
+	t_env_lst	*old_pwd;
+	t_env_lst	*pwd;
+
 	if (getcwd(path, 1024) == NULL)
 		perror("getcwd error\n");
 	pwd = find_env(cmd, "PWD");
@@ -68,17 +83,38 @@ void	cd(t_cmd *cmd, char **argv)
 		ft_putstr_fd("minishell: cd: ", 2);
 		perror(argv[1]);
 		exit_status = 1;
-		return ;
+		return (1);
 	}
 	free(pwd->value);
 	pwd->value = ft_strdup(new_path);
-	if (getcwd(path, 1024) == NULL)
-		perror("getcwd error\n");
-	free(pwd->value);
-	pwd->value = ft_strdup(path);
-	chdir(new_path);
-	free(new_path);
-	exit_status = 0;
+	return (0);
+}
+
+void	cd(t_cmd *cmd, char **argv)
+{
+	char		path[1024];
+	char		*new_path;
+	char		*tmp;
+	t_env_lst	*old_pwd;
+	t_env_lst	*pwd;
+
+	if (cd_home_check(cmd, argv))
+		return ;
+	if (argv[1][0] == '/')
+	{
+		if (chdir(argv[1]) == -1)
+		{
+			ft_putstr_fd("minishell: cd: ", 2);
+			perror(argv[1]);
+			exit_status = 1;
+			return ;
+		}
+		check_a_path(path, cmd);
+		return ;
+	}
+	if (check_r_path(path, cmd, argv, new_path))
+		return ;
+	refresh_path(path, cmd, new_path);
 }
 
 void	export(t_cmd *cmd, char **argv)
