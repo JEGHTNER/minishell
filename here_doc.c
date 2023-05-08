4fcd7e4fce940495f59eef8b3a4925f7cb5a0f9c
jeghtner <jeheon0717@naver.com>
2023-05-09 01:44:52 +0900

    <refactor> : here_doc refactor

--- here_doc.c
+++ here_doc.c
@@ -6,11 +6,29 @@
 /*   By: jehelee <jehelee@student.42.kr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/01 22:10:22 by jehelee           #+#    #+#             */
-/*   Updated: 2023/05/08 05:02:05 by jehelee          ###   ########.fr       */
+/*   Updated: 2023/05/09 01:43:39 by jehelee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+void	hd_read_line(char *limiter, int fd)
+{
+	char	*line;
+
+	while (1)
+	{
+		ft_putstr_fd("> ", STDOUT_FILENO);
+		line = get_next_line(STDIN_FILENO);
+		if (ft_strncmp(line, limiter, ft_strlen(limiter)) == 0)
+		{
+			free(line);
+			break ;
+		}
+		ft_putstr_fd(line, fd);
+		free(line);
+	}
+}
 
 void	here_doc_tmp(char *limiter, int index)
 {
@@ -25,18 +43,7 @@ void	here_doc_tmp(char *limiter, int index)
 	fd = open(file_name, O_WRONLY | O_CREAT | O_TRUNC, 0644);
 	if (fd == -1)
 		perror(file_name);
-	while (1)
-	{
-		ft_putstr_fd("> ", STDOUT_FILENO);
-		line = get_next_line(STDIN_FILENO);
-		if (ft_strncmp(line, limiter, ft_strlen(limiter)) == 0)
-		{
-			free(line);
-			break ;
-		}
-		ft_putstr_fd(line, fd);
-		free(line);
-	}
+	hd_read_line(limiter, fd);
 	close(fd);
 	free(file_name);
 }
